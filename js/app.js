2ab7172251be10a2eb00ad145c2c2ac8744dcaaf
Avi Tevet <github@avitevet.com>
2016-08-31 21:57:33 -0700

    Single enemy, always goes l2r.  Random speed, random y-lane during each pass. Player can move in all directions, and win. Still missing: collision detection

--- js/app.js
+++ js/app.js
@@ -1,12 +1,60 @@
+/* global Resources, ctx */
+const SPRITE_DIM = {
+	"x" : 101,
+	"y" : 171,
+	"tile_y" : 85
+};
+const BOARD_DIM = {
+	"width" : SPRITE_DIM.x * 5,
+	"height" : 535  // some of the blocks overlap vertically
+};
+const INITIAL_X = -SPRITE_DIM.x;
+const Y_LANES = [400, 315, 230, 145, 60];
+const MIN_SPEED = 100;
+const MAX_SPEED = 500;
+
+var GameObject = function(sprite, x, y) {
+	this.sprite = sprite;
+	this.x = x;
+	this.y = y;
+}
+
+GameObject.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+var randomRange = function(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  var sprite = 'images/enemy-bug.png';
+
+	// initial position for the enemy
+	var x = INITIAL_X;
+	var y = Y_LANES[randomRange(1, Y_LANES.length)];
+
+	GameObject.call(this, sprite, x, y);
+
+	this.newSpeed();
 };
+Enemy.prototype = Object.create(GameObject.prototype);
+Enemy.prototype.constructor = Enemy;
+
+Enemy.prototype.newSpeed = function() {
+	// speed at which the enemy moves.  It should be mostly slow-moving
+	// enemies, with a few fast moving enemies.  So, first pick a random number
+	// between min & max, which establishes an upper bound.  Then pick another
+	// number between min & upper, which becomes the speed.  This makes low speeds
+	// more likely to be selected than high speeds.
+	this.speed = randomRange(MIN_SPEED, randomRange(MIN_SPEED, MAX_SPEED));
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,23 +62,65 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+		this.x += this.speed * dt;
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+		if (this.x > BOARD_DIM.width + SPRITE_DIM.x) {
+			this.x = INITIAL_X;
+			this.y = Y_LANES[randomRange(1, Y_LANES.length)];
+			this.newSpeed();
+		}
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+	var sprite = 'images/char-boy.png';
+	var x = 2 * SPRITE_DIM.x,
+		y = Y_LANES[0];
+	GameObject.call(this, sprite, x, y);
+};
+Player.prototype = Object.create(GameObject.prototype);
+Player.prototype.constructor = Player;
 
+
+Player.prototype.update = function(dt) {
+
+};
+
+
+Player.prototype.win = function() {
+	// reset x & y
+	this.x = 2 * SPRITE_DIM.x;
+	this.y = Y_LANES[0];
+}
+
+Player.prototype.handleInput = function(dir) {
+	// check for valid move, then update x & y as necessary
+	if ((dir === "left") && (this.x !== 0)) {
+		this.x -= SPRITE_DIM.x;
+	}
+	else if ((dir === "right") && (this.x !== 4 * SPRITE_DIM.x)) {
+		this.x += SPRITE_DIM.x;
+	}
+	else if ((dir === "down") && (this.y !== Y_LANES[0])) {
+		this.y += SPRITE_DIM.tile_y;
+	}
+	else if (dir === "up") {
+		if (this.y !== Y_LANES[Y_LANES.length - 1]) {
+			this.y -= SPRITE_DIM.tile_y;
+		}
+		else {
+			this.win();
+		}
+	}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var player = new Player();
+var allEnemies = [new Enemy()];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
